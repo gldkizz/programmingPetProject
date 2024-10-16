26aa2773bf1fd689225e6e452ed544f7dd523772
gldkizz <stenlypaints@gmail.com>
2024-10-16 10:15:13 +0300

    caesarUpdate

--- cpp/caesar/main.cpp
+++ cpp/caesar/main.cpp
@@ -2,26 +2,34 @@
 
 using namespace std;
 
+// void caesarROT(string inputData, int rotNum) {
+//     for (char c: inputData) {
+//         if(isalpha(c)) {
+//             if(isupper(c)){
+//                 if(int(tolower(c)) + rotNum > 122) cout << char(toupper(97 + (rotNum - (123 - int(tolower(c))))));
+//                 else cout << char(toupper(int(tolower(c)) + rotNum));
+//             } else {
+//                 if(int(c) + rotNum > 122) cout << char((97 + (rotNum - (123 - int(c)))));
+//                 else cout << char(int(c) + rotNum);
+//             }}
+//         else if (isdigit(c)) 
+//             if (int(c) + rotNum > 57 && rotNum > 9) cout << char(int(c) + rotNum % 10);
+//             else if (int(c) + rotNum > 57 && rotNum < 10) cout << char(48 + (rotNum - (58 - int(c))));
+//             else cout << char(int(c) + rotNum);
+//         else cout << c;
+//     }
+// }
+
 void caesarROT(string inputData, int rotNum) {
-    for (char c: inputData) {
-        if(isalpha(c)) {
-            if(isupper(c)){
-                if(int(tolower(c)) + rotNum > 122) cout << char(toupper(97 + (rotNum - (123 - int(tolower(c))))));
-                else cout << char(toupper(int(tolower(c)) + rotNum));
-            } else {
-                if(int(c) + rotNum > 122) cout << char((97 + (rotNum - (123 - int(c)))));
-                else cout << char(int(c) + rotNum);
-            }}
-        else if (isdigit(c)) 
-            if (int(c) + rotNum > 57 && rotNum > 9) cout << char(int(c) + rotNum % 10);
-            else if (int(c) + rotNum > 57 && rotNum < 10) cout << char(48 + (rotNum - (58 - int(c))));
-            else cout << char(int(c) + rotNum);
+    for(char c: inputData) {
+        if(isalpha(c)) cout << char(97 + (int(c) - 97 + rotNum) % 26);
+        else if(isdigit(c)) cout << char(48 + (int(c) - 48 + rotNum) % 10);
         else cout << c;
     }
 }
 
 int main() {
-    caesarROT("Hello World!123", 24);
+    caesarROT("abcde1234", 23);
     cout << endl;
     return 0;
 }
